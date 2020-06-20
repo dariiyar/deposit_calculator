c8f3a957afb0a524cd9b79bdf32d2fea524a8ac2
Darii Yar <dariiy.yar@gmail.com>
2020-06-20 18:03:49 +0300

    fixes rubocop

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -3,7 +3,7 @@ class Deposit
 
   attr_accessor :currency, :amount, :interest_rate, :start_date, :period, :period_type
 
-  validates :currency, presence: true, inclusion: { in: CURRENCIES, message: '%<value> is not a valid type' }
+  validates :currency, presence: true, inclusion: { in: CURRENCIES, message: '%{value} is not a valid type' }
   validates :amount,
             presence: true,
             numericality: { greater_than_or_equal_to: 100, message: 'should be equal or bigger than 100' }
@@ -15,7 +15,7 @@ class Deposit
             numericality: { only_integer: true,
                             greater_than: 0,
                             message: 'should be number and equal or bigger than 1' }
-  validates :period_type, presence: true, inclusion: { in: PERIOD_TYPES, message: '%<value> is not a valid type' }
+  validates :period_type, presence: true, inclusion: { in: PERIOD_TYPES, message: '%{value} is not a valid type' }
   validate do
     DateTime.parse(start_date)
   rescue StandardError
